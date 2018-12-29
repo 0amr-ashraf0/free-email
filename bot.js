64108a739417e0b366e155a1c8bd798a010eb17e
0amr-ashraf0 <46089737+0amr-ashraf0@users.noreply.github.com>
2018-12-29 13:48:05 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const RichEmbed = require("discord.js");
+var prefix = ".";
+client.on('message', message => {//Toxic Codes
+    if (message.author.bot) return;
+     if (message.content === prefix + "email") {//Toxic Codes
+function randomem() {//Toxic Codes
+let email = '';//Toxic Codes
+const n3k4a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._"\'';
+for (let i = 0; i < 5; i++) email += n3k4a.charAt(Math.floor(Math.random() * n3k4a.length));//Toxic Codes
+return email//Toxic Codes
+}//Toxic Codes//Toxic Codes
+function randompass() {//Toxic Codes
+let pass = '';//Toxic Codes
+const Toxic Codes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?@#$%&()-_"\'';//Toxic Codes
+for (let i = 0; i < 8; i++) pass += Toxic Codes.charAt(Math.floor(Math.random() * Toxic Codes.length));//Toxic Codes
+return pass;
+}
+const random1 = randomem();//Toxic Codes//Toxic Codes
+const random2 = randompass();//Toxic Codes
+message.author.send(`------------------------//Toxic Codes
+email : **${random1}@gmail.com**//Toxic Codes
+password : **${random2}**//Toxic Codes
+------------------------`).catch(err => {//Toxic Codes
+   if(err == "DiscordAPIError: Cannot send messages to this user") {//Toxic Codes
+      return message.channel.send("**للأسف , لديك اعدادات خصوصية لاتسمح لي بأرسال رسائل خاصة لك **");//Toxic Codes
+}//Toxic Codes
+});//Toxic Codes
+//Toxic Codes
+message.channel.send("**تم الارسال الحساب في الخاص | ☑ **")//Toxic Codes
+});//Toxic Codes
+client.login("NTI4NTM4ODUzNjgyNzA4NDkx.DwjwZw.qrMr78evq8H47F-yP5qJ1I0C7dI")
